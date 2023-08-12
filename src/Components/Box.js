109210ec83619599b6f2671e758f1e0846a19eed
MacreenSmith <macreensmith@gmail.com>
2023-08-11 22:27:22 -0400

    Created a Box component and added it the main webpage as well as added styling to it.

---
+++ src/Components/Box.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Box = () => {
+  return (
+    <>
+        <div className="box">
+      <h2>1</h2>
+      <img src="./images/pikachu.png" alt ="" />
+      <h2>Pikachu</h2>
+      </div>
+    </>
+  )
+}
+
+export default Box
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,14 +1,14 @@
 import React from 'react'
-
+import Box from './Box'
 const Main = () => {
   return (
    <>
      <div className="container">
       <div>
-
+        <Box />
       </div>
       <div>
-        
+
       </div>
     </div>
    </>
