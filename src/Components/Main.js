d26d7758320cce86e95c59f39bc1ce16f57c8575
MacreenSmith <macreensmith@gmail.com>
2023-08-11 21:34:17 -0400

    First Commit

---
+++ src/Components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+   <>
+     <div className="container">
+      <div>
+
+      </div>
+      <div>
+        
+      </div>
+    </div>
+   </>
+  )
+}
+
+export default Main
