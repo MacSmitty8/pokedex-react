3cbdf170a15381703606b58059df14f8b19c92d3
MacreenSmith <macreensmith@gmail.com>
2023-08-13 22:19:50 -0400

    Added the pokeFun async function, and the getPokemon async function with parts of the data it needs

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -7,11 +7,23 @@ const Main = () => {
     const [pokeData, setPokeData] = useState([])
     const [loading, setLoading] = useState(true);
     const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
-
+    const [nextUrl, setNextUrl] = useState();
+    const [prevUrl, setPrevUrl] = useState();
+ 
     const pokeFun = async () => {
         setLoading(true)
         const res = await axios.get(url);
         console.log(res.data.results)
+        setNextUrl(res.data.next);
+        setPrevUrl(res.data.previous);
+        getPokemon(res.data.results);
+        setLoading(false);
+    }
+    const getPokemon = async(res)=>{
+        res.map(async (item) => {
+            const result = await axios.get(item.url)
+            console.log(result.data)
+        })
     }
     useEffect(() => {
         pokeFun()
